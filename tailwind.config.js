2ccf37c68f75955dd8b361b32566e364816343fd
nandartika <kartikanandap@gmail.com>
2024-06-03 11:20:17 +0700

    add: theme color

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    colors: {
+      primary: "#fff",
+      secondary: "#2d3e50",
+      gray: "#f4f4f5",
+    },
   },
   plugins: [],
 };
