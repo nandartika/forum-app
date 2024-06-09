abd59195b009a802828aaf9abee99f004e244ffa
nandartika <kartikanandap@gmail.com>
2024-06-09 18:11:22 +0700

    add: filtered threads by category

--- src/components/category/CategoryItem.jsx
+++ src/components/category/CategoryItem.jsx
@@ -1,8 +1,28 @@
 import PropTypes from "prop-types";
+import { useSearchParams } from "react-router-dom";
 
 export default function CategoryItem({ children, size = "sm" }) {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const categoryActive = searchParams.get("category") === children;
+  const isActive = categoryActive && size === "md";
+
+  const toggleClick = () => {
+    if (categoryActive) {
+      setSearchParams({});
+    } else {
+      setSearchParams({ category: children.toLowerCase() });
+    }
+  };
+
   return (
-    <p className={`w-fit rounded-md border px-2 py-1 text-${size}`}>
+    <p
+      className={
+        (isActive ? `bg-secondary text-primary` : "") +
+        " " +
+        `w-fit rounded-md border px-2 py-1 text-${size}`
+      }
+      onClick={toggleClick}
+    >
       #{children}
     </p>
   );
--- src/pages/threads/ThreadsList.jsx
+++ src/pages/threads/ThreadsList.jsx
@@ -1,15 +1,24 @@
 import { useSelector } from "react-redux";
 import ThreadItem from "./ThreadItem";
 import Indicator from "../../components/common/Indicator";
+import { useSearchParams } from "react-router-dom";
 
 export default function ThreadsList() {
+  const [searchParams] = useSearchParams();
   const threads = useSelector((state) => state.threads);
   const isThreadNotEmpty = threads.length > 0;
+
+  const category = searchParams.get("category");
+  const filteredThreads = category
+    ? threads.filter((thread) => thread.category === category)
+    : threads;
 
   return (
     <div className="mt-4 w-full">
       {isThreadNotEmpty ? (
-        threads.map((thread) => <ThreadItem key={thread.id} {...thread} />)
+        filteredThreads.map((thread) => (
+          <ThreadItem key={thread.id} {...thread} />
+        ))
       ) : (
         <Indicator />
       )}
